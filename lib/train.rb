4b739af1c8652259f8edbdf57c37b7f79b8a7975
maxim <maximsandulsky@gmail.com>
2021-04-08 04:05:37 +0300

    Restored

---
+++ lib/train.rb
@@ -0,0 +1,62 @@
+class Train
+  attr_reader :speed, :wagons, :number, :type, :route
+  
+  def initialize(number, type, wagons)
+    @number, @type, @wagons = number, type, wagons
+  end
+
+  def wagons_increase
+    if @speed == 0
+      @wagons += 1
+    end
+  end
+  
+  def wagons_decrease
+    if @speed == 0
+      @wagons -= 1
+    end
+  end
+  
+  def speed_gain(velocity)
+    @speed += velocity
+  end
+  
+  def stop
+    @speed = 0
+  end
+  
+  def train_route=(route_set)
+    @route = route_set
+    @current_station_index = 0
+    current_station.arriving_train(self)
+  end
+
+  def move_forward
+    if  next_station != nil
+      current_station.departure_train(self)
+      @current_station_index += 1
+      current_station.arriving_train(self)
+    end
+  end
+  
+  def move_backward
+    if  passed_station != nil
+      current_station.departure_train(self)
+      @current_station_index -= 1
+      current_station.arriving_train(self)
+    end
+  end
+  
+  def current_station
+    route.stations[@current_station_index]
+  end
+  
+  def passed_station
+    route.stations[@current_station_index - 1]
+  end
+  
+  def next_station
+    route.stations[@current_station_index + 1]
+  end
+  
+end
