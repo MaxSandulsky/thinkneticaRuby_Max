213fb5617cacb16305ddb59458c8a2d6a9e156e2
maxim <maximsandulsky@gmail.com>
2021-04-16 19:48:22 +0300

    Reforged after revision

--- lib/wagon.rb
+++ lib/wagon.rb
@@ -1,7 +1,7 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
-require_relative 'prodaction_company.rb'
+require_relative 'instance_counter.rb'
 require_relative 'manufacturer.rb'
 
 class Wagon
